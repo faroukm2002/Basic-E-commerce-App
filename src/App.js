5492534355827e2a2b3c15f015c5bc298d0066eb
faroukm2002 <faroukm238@gamil.com>
2023-12-29 23:52:41 +0200

    create react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Navbar from "./components/Navbar";
 import ProductsList from "./components/ProductsList";
 import Slider from "./components/Slider";
+import { Routes,Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Slider></Slider>
+<Routes>
+<Route path="/" element={
+  <>
+  <Slider></Slider>
       <ProductsList></ProductsList>
+  </>
+} ></Route>
+
+</Routes>
+
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Assuming Bootstrap 5
 
 import 'bootstrap/dist/js/bootstrap.min.js';
+import {BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
